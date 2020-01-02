b234bba6eb20db357af55bd3da3110549ac74e68
mervynyang(杨名远) <mervynyang@tencent.com>
2020-01-02 17:07:04 +0800

    fix: stub模板路径

--- src/Console/MakeApiException.php
+++ src/Console/MakeApiException.php
@@ -9,11 +9,11 @@ class MakeApiException extends GeneratorCommand
 {
     protected $signature = 'make:api-exception {name}';
 
-    protected $description = ' Create a new custom api exception class';
+    protected $description = 'Create a new custom api exception class';
 
     protected function getStub()
     {
-        return __DIR__.'../stubs/Exception.stub';
+        return __DIR__.'/../stubs/Exception.stub';
     }
 
      public function getDefaultNamespace($rootNamespace)
